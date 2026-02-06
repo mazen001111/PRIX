3335ac47c04b24cb16742af4ed1041a31d72d02a
mazen001111 <2300624@eng.asu.edu.eg>
2026-02-06 04:16:13 +0200

    error fix

--- src/store/LoginState.jsx
+++ src/store/LoginState.jsx
@@ -1,13 +1,16 @@
 import { create } from "zustand";
 import { persist } from "zustand/middleware";
+const isBrowser = typeof window !== "undefined";
 
 export const useLogin = create(persist((set) => ({
     Token: null,
     login: (token) => set(() => ({ Token: token })),
     logout: () => {
         set(() => ({ Token: null }))
-        localStorage.removeItem("useLogin")
-        sessionStorage.removeItem("useLogin")
+        if (isBrowser) {
+          localStorage.removeItem("useLogin");
+          sessionStorage.removeItem("useLogin");
+        }
     },
 }),
     {
